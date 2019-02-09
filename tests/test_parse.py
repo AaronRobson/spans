5f0e2debaccc14eb2feec5c29498173d21b6bc60
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-02-09 22:02:01 +0000

    handle duplicates and ordering by returning set from parse_numbers

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -8,16 +8,22 @@ from spans import parse_numbers
 class TestParseNumbers(unittest.TestCase):
 
     def test_empty(self):
-        self.assertEqual(parse_numbers(''), [])
+        self.assertEqual(parse_numbers(''), set())
 
     def test_comma_separated(self):
-        self.assertEqual(parse_numbers('1,2'), [1, 2])
+        self.assertEqual(parse_numbers('1,2'), {1, 2})
 
     def test_comma_space_separated(self):
-        self.assertEqual(parse_numbers('3, 4'), [3, 4])
+        self.assertEqual(parse_numbers('3, 4'), {3, 4})
 
     def test_space_separated(self):
-        self.assertEqual(parse_numbers('5 6'), [5, 6])
+        self.assertEqual(parse_numbers('5 6'), {5, 6})
 
     def test_whitespace_separated(self):
-        self.assertEqual(parse_numbers('7 \t \n 8'), [7, 8])
+        self.assertEqual(parse_numbers('7 \t \n 8'), {7, 8})
+
+    def test_duplicates(self):
+        self.assertEqual(parse_numbers('9,9'), {9})
+
+    def test_incorrect_order(self):
+        self.assertEqual(parse_numbers('11,10'), {10, 11})
