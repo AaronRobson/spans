6babdd0037b96f18154939b58758d1eff396ee7d
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-07-29 23:26:01 +0100

    property test of simplify

--- tests/test_consistent.py
+++ tests/test_consistent.py
@@ -3,10 +3,15 @@ import unittest
 from hypothesis import given
 from hypothesis.strategies import sets, integers
 
-from spans import encode, decode
+from spans import encode, decode, simplify
 
 
 class TestConsistent(unittest.TestCase):
     @given(sets(integers()))
-    def test_numbers_to_spans_and_back_should_be_the_same(self, values):
+    def test_decode_inverts_encode(self, values):
         self.assertEqual(decode(encode(values)), values)
+
+    @given(sets(integers()))
+    def test_encode_returns_simplest_result(self, values):
+        numbers = encode(values)
+        self.assertEqual(simplify(numbers), numbers)
