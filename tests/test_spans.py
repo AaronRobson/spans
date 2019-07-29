8adbcd4a61014ac3d01ea9ff4b2a49effd57f6cf
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-07-30 00:02:41 +0100

    factor TestSpans single method into multiple focussed ones

--- tests/test_spans.py
+++ tests/test_spans.py
@@ -13,24 +13,24 @@ _s3_4 = Span(*_givenNumbers3_4)
 
 
 class TestSpans(unittest.TestCase):
-
-    def testSpanObject(self):
+    def test_single_number(self):
         self.assertEqual(_s1.start, _givenNumbers1[0])
-        self.assertEqual(_s3_4.start, _givenNumbers3_4[0])
-
         self.assertEqual(_s1.finish, _givenNumbers1[0])
-        self.assertEqual(_s3_4.finish, _givenNumbers3_4[1])
-
         self.assertTrue(_s1.single)
-        self.assertFalse(_s3_4.single)
-
         self.assertEqual(_s1.arguments, _givenNumbers1)
+
+    def test_range_of_numbers(self):
+        self.assertEqual(_s3_4.start, _givenNumbers3_4[0])
+        self.assertEqual(_s3_4.finish, _givenNumbers3_4[1])
+        self.assertFalse(_s3_4.single)
         self.assertEqual(_s3_4.arguments, _givenNumbers3_4)
 
+    def test_equality(self):
         self.assertEqual(_s1, _s1)
         self.assertNotEqual(_s1, _s3_4)
         self.assertEqual(_s3_4, _s3_4)
 
+    def test_repr(self):
         self.assertEqual(
             repr(_s1),
             'Span(%s)' % (str(_givenNumbers1[0])))
@@ -41,12 +41,14 @@ class TestSpans(unittest.TestCase):
                 ', ' +
                 str(_givenNumbers3_4[1])))
 
+    def test_str(self):
         self.assertEqual(str(_s1), str(_givenNumbers1[0]))
         self.assertEqual(
             str(_s3_4),
             str(_givenNumbers3_4[0]) + '-' +
             str(_givenNumbers3_4[1]))
 
+    def test_iter(self):
         self.assertEqual(tuple(iter(Span(5, 7))), (5, 6, 7))
 
 
