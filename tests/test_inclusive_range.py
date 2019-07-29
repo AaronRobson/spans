97e1c663be0e31cc06723c03ef5136441217fc8e
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-07-30 00:29:15 +0100

    test inclusive_range with single values and backwards ranges

--- tests/test_inclusive_range.py
+++ tests/test_inclusive_range.py
@@ -6,5 +6,11 @@ from spans import inclusive_range
 
 
 class TestInclusiveRange(unittest.TestCase):
+    def test_single(self):
+        self.assertEqual(inclusive_range(3), [3])
+
     def test_forward(self):
         self.assertEqual(inclusive_range(5, 7), [5, 6, 7])
+
+    def test_backward(self):
+        self.assertEqual(inclusive_range(10, 8), [8, 9, 10])
